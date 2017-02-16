257bc6b4853f1cfa6e37c7b67000a7ab398a6094
Ashlee Christine Hayes <ACH@Ashlees-MacBook-Pro.local>
2017-02-15 18:04:42 -0800

    beginning build

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+require "header.php"; ?>
+<nav class="topnav">
+  <ul>
+    <li><a href="#portfolio">PORTFOLIO</a></li>
+    <li><a href="#skills">SKILLS</a></li>
+    <li><a href="#about">ABOUT</a></li>
+    <li><a href="#contact">CONTACT</a></li>
+    <li class="icon">
+	    <a href="javascript:void(0);" id="icon_link" >&#9776;</a>
+    </li>
+  </ul>
+</nav>
+<section class="front page layer">
+  <h1>Hover</h1>
+</section>
+
+<?php require "footer.php"; ?>
